2d2063a5c8039b14608f54cb6de472b75862179a
Alexander Doucette <alexander.p.doucette@gmail.com>
2020-03-03 20:57:25 +0000

    Automatically backed up by Learn

--- def.rb
+++ def.rb
@@ -1,3 +1,11 @@
+# With methods you are able to shorten the end result of the code by doing the work once and calling it back later.  I don't think anybody wants to keep typing the same thing over and over.  
+
+
+
+
+
+
+
 def greeting
   phrase = "Hello World!!!"
   puts phrase
